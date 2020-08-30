884f3c0b9b2a975e00e2fd0821579d0c26443152
Amit Agarwal <amit@labnol.org>
2020-08-30 16:32:24 +0530

    Rich Text Emails 👱🏻‍♂️

---
+++ google-apps-script/rich-text/README.md
@@ -0,0 +1,5 @@
+# Google Spreadsheet to HTML Email
+
+[Send Rich Text Emails from Google Spreadsheet](https://www.labnol.org/send-rich-text-emails-200830)
+
+> This software is provided "as-is," without any express or implied warranty.
---
+++ google-apps-script/rich-text/code.js
@@ -0,0 +1,66 @@
+/**
+ * Rich Text to Email
+ * ========================
+ *
+ * Written by Amit Agarwal
+ * Email: amit@labnol.org
+ * Web: https://www.labnol.org
+ * Twitter: @labnol
+ *
+ * Under MIT License
+ */
+
+const sendRichEmail = () => {
+  const cellAddress = "A1";
+  const sheetName = "Mail Merge";
+  const recipient = "amit@labnol.org";
+
+  const richTextValue = SpreadsheetApp.getActiveSpreadsheet()
+    .getSheetByName(sheetName)
+    .getRange(cellAddress)
+    .getRichTextValue();
+
+  /* Run is a stylized text string used to represent cell text.
+     This function transforms the run into HTML with CSS
+   */
+  const getRunAsHtml = (richTextRun) => {
+    const richText = richTextRun.getText();
+
+    // Returns the rendered style of text in a cell.
+    const style = richTextRun.getTextStyle();
+
+    // Returns the link URL, or null if there is no link
+    // or if there are multiple different links.
+    const url = richTextRun.getLinkUrl();
+
+    const styles = {
+      color: style.getForegroundColor(),
+      "font-family": style.getFontFamily(),
+      "font-size": `${style.getFontSize()}pt`,
+      "font-weight": style.isBold() ? "bold" : "",
+      "font-style": style.isItalic() ? "italic" : "",
+      "text-decoration": style.isUnderline() ? "underline" : "",
+    };
+
+    // Gets whether or not the cell has strikethrough.
+    if (style.isStrikethrough()) {
+      styles["text-decoration"] = `${styles["text-decoration"]} line-through`;
+    }
+
+    const css = Object.keys(styles)
+      .filter((attr) => styles[attr])
+      .map((attr) => [attr, styles[attr]].join(":"))
+      .join(";");
+
+    const styledText = `<span style='${css}'>${richText}</span>`;
+    return url ? `<a href='${url}'>${styledText}</a>` : styledText;
+  };
+
+  /* Returns the Rich Text string split into an array of runs,
+  wherein each run is the longest possible
+  substring having a consistent text style. */
+  const runs = richTextValue.getRuns();
+  const htmlBody = runs.map((run) => getRunAsHtml(run)).join("");
+
+  MailApp.sendEmail(recipient, "Rich HTML Email", "", { htmlBody });
+};
