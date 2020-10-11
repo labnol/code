7cb75639c5c36d2274abde0156972808fd4397d0
Amit Agarwal <amit@labnol.org>
2020-10-11 14:03:36 +0530

    Read-only Google Drive

---
+++ google-apps-script/drive-lock/README.md
@@ -0,0 +1,3 @@
+# Google Drive Lock 🐢
+
+[Make files Read-only in Google Drive](https://www.labnol.org/code/read-only-google-drive-file-201011)
---
+++ google-apps-script/drive-lock/code.js
@@ -0,0 +1,26 @@
+/**
+ *  Make Google Drive files Read only
+ *  Author: amit@labnol.org
+ *  Web: https://digitalinspiration.com/
+ *  MIT License
+ **/
+
+const makeFileReadyOnly = () => {
+  const fileUrl = "https://docs.google.com/document/d/.....";
+  const [fileId] = fileUrl.split("/").filter((e) => /[_-\w]{25,}/.test(e));
+  UrlFetchApp.fetch(`https://www.googleapis.com/drive/v3/files/${fileId}`, {
+    method: "PATCH",
+    contentType: "application/json",
+    headers: {
+      Authorization: `Bearer ${ScriptApp.getOAuthToken()}`,
+    },
+    payload: JSON.stringify({
+      contentRestrictions: [
+        {
+          readOnly: true,
+          reason: "Prevent accidental editing",
+        },
+      ],
+    }),
+  });
+};
